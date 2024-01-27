d49c4915708618ae242c352af1f444a73d99d22e
Fahad Ahmed Khan <fahadnishad124@gmail.com>
2024-01-27 23:43:26 +0500

    complete the delete listing functionality of  user

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -32,7 +32,7 @@ const Profile = () => {
   const [updateSuccess, setUpdateSuccess] = useState(false);
   const [showListingsError, setShowListingError] = useState(false);
   const [userListings, setUserListings] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
   const dispatch = useDispatch();
 
   // console.log(file)
@@ -164,11 +164,28 @@ const Profile = () => {
       setUserListings(data);
     } catch (error) {
       setShowListingError(true);
-    }
-    finally {
+    } finally {
       setIsLoading(false);
     }
   };
+  const handleListingDelete = async (listingId) =>{
+    try {
+
+      const res = await fetch(`/api/listing/delete/${listingId}`,{
+        method:'DELETE',
+
+      });
+      const data = await res.json();
+      if(data.success === false)
+      {
+        console.log(data.message)
+        return
+      } 
+      setUserListings((prev)=>prev.filter((listing)=>listing._id !== listingId))  
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -259,7 +276,9 @@ const Profile = () => {
       <p className="text-red-700 mt-5">
         {showListingsError ? "Error Show Listings" : ""}
       </p>
-      {isLoading && <p className="text-green-700 text-center font-semibold">Loading...</p>}
+      {isLoading && (
+        <p className="text-green-700 text-center font-semibold">Loading...</p>
+      )}
       {userListings && userListings.length > 0 && (
         <div className="flex flex-col gap-4">
           <h1 className="text-center mt-7 text-3xl font-semibold">
@@ -285,8 +304,13 @@ const Profile = () => {
               </Link>
 
               <div className="flex flex-col items-center gap-2">
-                <button className="text-white uppercase bg-red-700 p-2 rounded-lg w-full hover:opacity-95">Delete</button>
-                <button className="text-white uppercase bg-green-700 p-2 rounded-lg w-full hover:opacity-95">Edit</button>
+                <button onClick={()=>handleListingDelete(listing._id)} className="text-white
+                 uppercase bg-red-700 p-2 rounded-lg w-full hover:opacity-95">
+                  Delete
+                </button>
+                <button className="text-white uppercase bg-green-700 p-2 rounded-lg w-full hover:opacity-95">
+                  Edit
+                </button>
               </div>
             </div>
           ))}
