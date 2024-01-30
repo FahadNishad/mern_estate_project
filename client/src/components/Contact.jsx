bfdb8880d99f281263652a2756ec6ab406550edb
Fahad Ahmed Khan <fahadnishad124@gmail.com>
2024-01-30 20:59:42 +0500

    add contact lanlord functionality to the listing page

---
+++ client/src/components/Contact.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Contact = ({ listing }) => {
+  const [landlord, setLandlord] = useState(null);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const fetchLandLord = async () => {
+      try {
+        const res = await fetch(`/api/user/${listing.userRef}`);
+        const data = await res.json();
+        setLandlord(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchLandLord();
+  }, [listing.userRef]);
+  const onchange = (e) => {
+    setMessage(e.target.value);
+  };
+  return (
+    <>
+      {landlord && (
+        <div className="flex flex-col gap-2">
+          <p>
+            Contact <span className="font-semibold">{landlord.username}</span>{" "}
+            for{" "}
+            <span className="font-semibold">{listing.name.toLowerCase()}</span>
+          </p>
+          <textarea
+            name="message"
+            id="message"
+            ows="2"
+            value={message}
+            onChange={onchange}
+            placeholder="Enter Your Message here"
+            className="w-full border p-3 rounded-lg"
+          ></textarea>
+          <Link
+            to={`mailto:${landlord.email}?Subject=Regarding${listing.name}&body=${message}`}
+            className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95"
+          >
+            Send Message
+          </Link>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Contact;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,28 +1,22 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { useState } from 'react'
-import OAuth from '../components/OAuth'
-
-
+import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import {  useNavigate } from 'react-router-dom';
 const SignUp = () => {
-
-  const [formData, setFormData] = useState({})
-
+  const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
-  const [loading, setLoading]= useState(false);
-  const navigate= useNavigate();
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
   };
-  
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      dispatch(signInStart());
-      const res = await fetch('/api/auth/signin', {
+      setLoading(true);
+      const res = await fetch('/api/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -31,32 +25,52 @@ const SignUp = () => {
       });
       const data = await res.json();
       console.log(data);
-  
       if (data.success === false) {
-        dispatch(signInFailure(data.message));
+        setLoading(false);
+        setError(data.message);
         return;
       }
-  
-      dispatch(signInSuccess(data));
-      navigate('/');
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in');
     } catch (error) {
-      dispatch(signInFailure(error.message));
+      setLoading(false);
+      setError(error.message);
     }
   };
-  
   console.log(formData)
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form onSubmit={handleSubmit} action="" className='flex flex-col gap-4 '>
-        <input type="text" placeholder='username' className='border p-3 rounded-lg'  id='username' onChange={handleChange}/>
-        <input type="email" placeholder='email' className='border p-3 rounded-lg'  id='email' onChange={handleChange}/>
-        <input type="password" placeholder='password' className='border p-3 rounded-lg'  id='password' onChange={handleChange}/>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='text'
+          placeholder='username'
+          className='border p-3 rounded-lg'
+          id='username'
+          onChange={handleChange}
+        />
+        <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+          onChange={handleChange}
+        />
 
-        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-          {loading ? 'Loading...':'Sign Up'}
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign Up'}
         </button>
-        <OAuth/>
       </form>
 
        <div className='flex gap-2 mt-5'>
