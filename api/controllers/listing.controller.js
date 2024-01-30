94e490e1a77d12feb734e5b85bb916732dce352c
Fahad Ahmed Khan <fahadnishad124@gmail.com>
2024-01-30 21:59:56 +0500

    create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,10 +1,9 @@
 import Listing from "../models/listing.model.js";
-import { errorHandler } from '../utils/error.js';
+import { errorHandler } from "../utils/error.js";
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
     return res.status(201).json(listing);
-
   } catch (error) {
     next(error);
   }
@@ -29,35 +28,80 @@ export const deleteListing = async (req, res, next) => {
 };
 
 export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found!"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only update your own listings!"));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getListing = async (req, res, next) => {
+  try {
     const listing = await Listing.findById(req.params.id);
     if (!listing) {
-      return next(errorHandler(404, 'Listing not found!'));
+      return next(errorHandler(404, "Listing not found!"));
     }
-    if (req.user.id !== listing.userRef) {
-      return next(errorHandler(401, 'You can only update your own listings!'));
-    }
-  
-    try {
-      const updatedListing = await Listing.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        { new: true }
-      );
-      res.status(200).json(updatedListing);
-    } catch (error) {
-      next(error);
-    }
-  };
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
 
-  export const getListing = async (req, res, next) => {
-    try {
-      const listing = await Listing.findById(req.params.id);
-      if (!listing) {
-        return next(errorHandler(404, 'Listing not found!'));
-      }
-      res.status(200).json(listing);
-    } catch (error) {
-      next(error);
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+
+    if (offer === undefined || offer === "flase") {
+      offer = { $in: [false, true] };
     }
-    
+
+    let furnished = req.body.furnished;
+    if (furnished === undefined || furnished === "flase") {
+      furnished = { $in: [false, true] };
     }
+
+    let parking = req.body.parking;
+    if (parking === undefined || parking === "flase") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.body.type;
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+
+    const sort = req.query.sort || "createAt";
+
+    const order = req.body.order || "desc";
+
+    const listing = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    }).sort({ [sort]: order }).limit(limit).skip(startIndex);
+
+
+    return res.status(200).json(listing)
+  } catch (error) {
+    next(error);
+  }
+};
